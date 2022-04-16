f3209c9db7e7913482d7768ef9c99713aa87a9f4
Lore <kanelillym@riseup.net>
2022-04-15 20:13:15 -0700

    initial commit

---
+++ plurpoll.py
@@ -0,0 +1,28 @@
+def get_question():
+    return input("What are we trying to decide? ")
+
+def get_responses():
+    response_list = []
+    response = "garbage starting data"
+    while response != "":
+        response = input("Response option: ")
+        if response == "":
+            break
+        response_list.append(response)
+    return response_list
+
+def get_votes():
+    pass
+
+def display_results():
+    pass
+
+def main():
+    question = get_question()
+    responses = get_responses()
+    print(question)
+    print(responses)
+    get_votes()
+    display_results()
+
+main()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# PlurPoll
+_An internal survey tool for plural systems._
+
+Conveniently also a way for me to learn to use python's pattern matching!
+
+On running the program, the input is prompted for a question, then any number of responses. Responses are ended by submitting an empty string.
+Voters can vote with "vote [-n $name] <response numbers>". duplicate names will not be rejected.
